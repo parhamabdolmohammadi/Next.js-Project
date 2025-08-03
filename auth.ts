59644f0d17a61c4682e8ecdf5876653b9a251b42
Parham Abdolmohammadi <parham.abdo@gmail.com>
2025-08-02 21:32:36 -0700

    falsy Accessing Session on the server

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
   providers: [
   GoogleProvider({
@@ -10,6 +10,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
   })
 ]
-})
+}
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions)
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import getServerSession from "next-auth";
 
 export default function Home() {
+  getServerSession(authOptions);
   return (
     <main>
       <h1>Hello World</h1>
