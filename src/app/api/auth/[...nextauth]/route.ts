403ca47a5eb6097be8ccc7a2376c6c4aec8f77c3
Parham Abdolmohammadi <parham.abdo@gmail.com>
2025-08-03 00:14:15 -0700

    Configuring Credentials Provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,40 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialProvider from "next-auth/providers/credentials";
 import prisma from "@/prisma/client";
+import bcrypt from 'bcrypt'
 
-const handler :NextAuthOptions = NextAuth({
+const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password", placeholder: "Password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({ where: { email: credentials.email } });
+
+        if (!user) return null;
+        // You should return the user object if authentication is successful
+        const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!)
+
+        return passwordsMatch ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session:
-    {
-      strategy: "jwt",
-    },
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
