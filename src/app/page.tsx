8fe17af420c2faed97e7a9a0404ace3022dbcc30
Parham Abdolmohammadi <parham.abdo@gmail.com>
2025-08-02 21:44:01 -0700

    fix accessing session on the server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
-import getServerSession from "next-auth";
+import { getServerSession } from "next-auth"; // ✅ Correct for v4
+import { authOptions } from "@/auth";
+import type { Session } from "next-auth"; // optional, for TS
 
-export default function Home() {
-  getServerSession(authOptions);
+export default async function Home() {
+  const session = (await getServerSession(authOptions)) as Session | null;
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>
+        Hello {session?.user?.name ? <span>{session.user.name}</span> : null}
+      </h1>
       <Link href="/users"> Users </Link>
       <ProductCard />
     </main>
